4a71c15fdd317bbb21c52695f6a3dd49f9e5ce59
Varun Thakur <vrnthkr969@gmail.com>
2016-12-25 13:14:14 +0000

    DLL added

--- DLL.cc
+++ DLL.cc
@@ -1,9 +1,151 @@
 #include<iostream>
 using namespace std;
 
+class node
+{
+    public:
+    int data;
+    node *next;
+    node *prev;
+    
+    node(int v,node *n=0,node *p=0){
+        data=v;
+        next=n;
+        prev=p;
+    }
+}*head;
+//implementation
+void insertAtHead(int x){
+    if(head==NULL)
+        head=new node(x);
+    else 
+    {
+        node *tmp=new node(x,head);
+        head->prev=tmp;
+        head=tmp;
+    }
+}
+void traverse(){
+    node *p=head;
+    
+    while(p)
+    {
+        cout<<p->data<<"->";
+        p=p->next;
+    }
+    cout<<"NULL"<<endl;    
+}
 
+void insertAtEnd(int x){
+    if(head==NULL)
+    head=new node(x);
+    else {
+        node *p=head;
+        while(p->next)
+            p=p->next;
+        node *tmp=new node(x,0,p);
+        p->next=tmp;
+    }
+}
+
+void insertAtPos(int x,int pos){
+    int k=1;
+    node *tmp=new node(x);
+    if(pos==1){
+        tmp->next=head;
+        head=tmp;
+    }
+    else
+    {
+        node *p=head;
+        node *q;
+        while(k<pos && p)
+            {
+                q=p;
+                p=p->next;
+                k++;
+            }
+            
+        q->next=tmp;
+        tmp->prev=q;
+        tmp->next=p;
+        if(p)
+        p->prev=tmp;
+        
+    }
+    
+}
+void deleteAtEnd(){
+    if(head==NULL)
+        cout<<"List is empty\n";
+    else if(head->next==NULL)
+    {
+        delete head;
+        head=NULL;
+    }
+    else
+    {
+        node *p=head;
+        node *q;
+        while(p->next){
+            q=p;
+            p=p->next;
+        }
+        
+        q->next=0;
+        p->prev=0;
+        delete p;
+    }
+}
+
+void deleteAtPos(int pos){
+    int k=1;
+    node *tmp;
+    if(pos==1){
+        tmp=head;
+        head=head->next;
+    }
+    else
+    {
+        node *p=head;
+        node *q;
+        while(k<pos && p)
+            {
+                q=p;
+                p=p->next;
+                k++;
+            }
+            if(p==NULL)
+                cout<<"Position doesnt exist\n";
+            else
+            {
+            tmp=p;
+            q->next=p->next;
+            if(p->next)
+                p->next->prev=q;
+            delete tmp;
+            }
+    }
+    
+}
 int main(){
         
-    cout<<"Hello World\n";
+    insertAtHead(1);
+    insertAtHead(2);
+    insertAtHead(3);
+    traverse();
+    insertAtEnd(4);
+    insertAtEnd(5);
+    insertAtEnd(6);
+    traverse();
+    insertAtPos(8,1);
+    insertAtPos(9,8);
+    traverse();
+    deleteAtEnd();
+    deleteAtEnd();
+    traverse();
+    deleteAtPos(1);
+    deleteAtPos(6);
+    traverse();
     return 0;
 }
--- SLL.cc
+++ SLL.cc
@@ -1,4 +1,4 @@
-//Single LinkedList Implementation
+//Single LinkedList Implementation and problems
 
 #include <iostream>
 using namespace std;
@@ -162,9 +162,82 @@ void deleteList(){
     }
     head=NULL;
 }
+
+
+
+//problems
+void reverseIterative()
+{
+    
+    node *p=head;
+    node *q;
+    node *r=0;
+    while(p){
+        q=p;
+        p=p->next;
+        q->next=r;
+        r=q;
+        
+    }
+    head=r;
+}
+
+node* reverseRecursive(node *head){
+    if(head==0 || head->next==0)
+    return head;
+    
+    node *second=head->next;
+    head->next=0;
+    node *rest=reverseRecursive(second);
+    second->next=head;
+    return rest;
+    
+}
+
+node* nthNodeFromEnd(int n){
+    node *pnthNode=0;
+    node *ptemp=head;
+    for(int i=1;i<n;i++)
+        if(ptemp)
+            ptemp=ptemp->next;
+    while(ptemp){
+        if(pnthNode==0)
+            pnthNode=head;
+        else pnthNode=pnthNode->next;
+        ptemp=ptemp->next;
+    }
+    if(pnthNode)
+    return pnthNode;
+
+    return NULL;
+}
+
+
+void loopIntroduce(){
+    node *p=head;
+    while(p->next)
+        p=p->next;
+    p->next=head;
+}
+
+int checkForLoop(){
+    node *slow=head;
+    node *fast=head;
+    
+    while(slow && fast && fast->next){
+        
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)
+        return 1;
+    }
+    
+    return 0;
+}
+
 int main() {
     
-    
+/*    
    insertAtHead(1);
    insertAtHead(2);
    insertAtHead(3);
@@ -190,7 +263,25 @@ int main() {
    
    deleteAtPos(5);
    traverse();
-   deleteList();
+   //deleteList();
+   //traverse();
+   reverseIterative();
    traverse();
+   head=reverseRecursive(head);
+   traverse();
+   */
+   insertAtEnd(1);
+   insertAtEnd(2);
+   insertAtEnd(3);
+   insertAtEnd(4);
+   insertAtEnd(5);
+   insertAtEnd(6);
+   traverse();
+   cout<<nthNodeFromEnd(2)->data<<endl;
+   loopIntroduce();
+   if(checkForLoop())
+   cout<<"Yes loop is present\n";
+   else cout<<"No\n";
+   
    
 }
