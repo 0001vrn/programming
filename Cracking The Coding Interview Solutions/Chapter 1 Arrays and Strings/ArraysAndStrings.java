a5a4f41f7d2e3a4645e81cb352f2e51d9c957060
Varun Thakur <vrnthkr969@gmail.com>
2018-02-25 09:37:48 +0000

    solved till 1.3

--- Cracking The Coding Interview Solutions/Chapter 1 Arrays and Strings/ArraysAndStrings.java
+++ Cracking The Coding Interview Solutions/Chapter 1 Arrays and Strings/ArraysAndStrings.java
@@ -1,7 +1,13 @@
 /*
 
-Implement an algorithm to determine if a string has all unique characters. What if
-you can not use additional data structures?
+1.1 Implement an algorithm to determine if a string has all unique characters. What if
+    you can not use additional data structures?
+
+1.3 Design an algorithm and write code to remove the duplicate characters in a string
+    without using any additional buffer. NOTE: One or two additional variables are fine.
+    An extra copy of the array is not.
+    FOLLOW UP
+    Write the test cases for this method.
 
 by    : Varun Thakur
 dated : 23-02-2018
@@ -10,6 +16,7 @@ import java.util.Arrays;
 
 
 public class ArraysAndStrings{
+    //region Answer to 1.1
     
     //approach 1 - use extra space
     public static boolean isUniqueChars2(String str){
@@ -97,9 +104,86 @@ public class ArraysAndStrings{
     //Space Complexity - No extra space
     //where n is the length of string
     
+    //endregion
     
+    //region Answer to 1.2
+    
+    public static void removeDuplicates(char[] str){
+        if(str==null) return;
+        int len = str.length;
+        if(len<2)
+            return;
+            
+        int tail = 1;
+        for(int i=1;i<len;i++){
+            
+            int j;
+            
+            for(j=0;j<tail;j++)
+                if(str[i]==str[j])
+                    break;
+                    
+            if (j == tail) {
+                str[tail] = str[i];
+                ++tail;
+            }
+        }
+        
+        str[tail] = 0;
+    }
+    //Time Complexity - O(n^2)
+    //Space Complexity - O(1)
+    /*
+    
+    Test Cases:
+    
+    1. String does not contain any duplicates, e.g.: abcd
+    2. String contains all duplicates, e.g.: aaaa
+    3. Null string
+    4. String with all continuous duplicates, e.g.: aaabbb
+    5. String with non-contiguous duplicate, e.g.: abababa
+    
+    */
+    
+    
+    public static void removeDuplicatesEff(char[] str) {
+        if (str == null) return;
+        int len = str.length;
+        if (len < 2) return;
+        boolean[] hit = new boolean[256];
+        for (int i = 0; i < 256; ++i) {
+            hit[i] = false;
+        }
+        hit[str[0]] = true;
+        int tail = 1;
+        for (int i = 1; i < len; ++i) {
+            if (!hit[str[i]]) {
+                str[tail] = str[i];
+                ++tail;
+                hit[str[i]] = true;
+            }
+        }
+        str[tail] = 0;
+    }
+    //Time Complexity - O(n)
+    //Space Complexity - Constant
+    /*
+    
+    Test Cases:
+    
+    1. String does not contain any duplicates, e.g.: abcd
+    2. String contains all duplicates, e.g.: aaaa
+    3. Null string
+    4. Empty string
+    5. String with all continuous duplicates, e.g.: aaabbb
+    6. String with non-contiguous duplicates, e.g.: abababa
+    
+    */
+    
+    //endregion
     public static void main(String[] args){
         
+        //region sample testcases for 1.1
         System.out.println(isUniqueChars2("BACD"));
         System.out.println(isUniqueChars2("BACCD"));
         
@@ -111,6 +195,15 @@ public class ArraysAndStrings{
         
         System.out.println(isUniqueCharsSorting("BACD"));
         System.out.println(isUniqueCharsSorting("BACCD"));
+        //endregion
+        
+        //String[] strTbl = {"abcd", "aaaa", "", "aaabbb", "abababa"};
+        
+        char[] str= "aabcd".toCharArray();
+        System.out.println(str);
+        removeDuplicatesEff(str);
+        System.out.println(str);
+        
         
     }
 }
